662406739bf741a3dd6cf574cde009a22ba5f8a0
nomedb <noemdb@gmail.com>
2024-04-15 09:12:13 -0400

    wip 2024-04-15_09:12:13

--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -1,3 +1,11 @@
 <div>
     <x-input wire:model="firstName" label="Name" placeholder="User's first name" />
+
+    <x-select
+        label="Select Status"
+        placeholder="Select one status"
+        :options="['Active', 'Pending', 'Stuck', 'Done']"
+        wire:model.defer="model"
+    />
+
 </div>
