a7f356fa97aaa8dc52e3f3f63f4c8d74276230a5
nomedb <noemdb@gmail.com>
2024-04-15 09:54:25 -0400

    wip 2024-04-15_09:54:25

--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -7,5 +7,14 @@
         :options="['Active', 'Pending', 'Stuck', 'Done']"
         wire:model.defer="model"
     />
+    <hr>
+    <!-- Declare a JavaScript object of data... -->
+    <div x-data="{ count: 0 }">
+        <!-- Render the current "count" value inside an element... -->
+        <h2 x-text="count"></h2>
+    
+        <!-- Increment the "count" value by "1" when a click event is dispatched... -->
+        <button x-on:click="count++">+</button>
+    </div>
 
 </div>
