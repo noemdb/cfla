957424c8a9c489aa36c6364d7f7545b548fc5618
nomebd <noemdb@gmail.com>
2024-12-04 10:19:34 -0400

    wip 2024-12-04_10:19:34

--- resources/views/livewire/home/modal/post.blade.php
+++ resources/views/livewire/home/modal/post.blade.php
@@ -22,7 +22,7 @@
 
     @if ($post->saefl_image_url)                            
         <div id="footer-out" class="flex justify-center">
-            <img src="{{asset($post->saefl_image_url)}}" class="border-b-2 block w-full max-w-48 rounded-lg shadow-lg" alt="Wild Landscape" />
+            <img src="{{asset($post->saefl_image_url)}}" class="border-b-2 block rounded-lg shadow-lg" alt="Wild Landscape" />
         </div> 
     @endif 
 
