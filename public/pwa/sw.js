e5bfdef2cb13daaa72f1abc950bf36a9bb85b262
nomebd <noemdb@gmail.com>
2025-01-28 11:51:31 -0400

    wip 2025-01-28_11:51:31

--- public/pwa/sw.js
+++ public/pwa/sw.js
@@ -1,45 +1,23 @@
-const CACHE_NAME = 'pwa-saefl-cache-v1.1.1'; // Cambia el número de versión
+const CACHE_NAME = 'saefl-cache-v1.1.2';
 const urlsToCache = [
-    '/',
-    '/index.html',
-    '/css/styles.css',
-    '/js/script.js',
-    '/icon.png'
+  '/',
+  '/styles.css',
+  '/app.js',
+  '/icon_48.png',
+  '/icon_128.png',
+  '/icon_512.png'
 ];
 
-// Instalar Service Worker y actualizar caché
-self.addEventListener('install', event => {
+self.addEventListener('install', (event) => {
     event.waitUntil(
-        caches.open(CACHE_NAME).then(cache => {
-            return cache.addAll(urlsToCache);
-        })
+        caches.open(CACHE_NAME)
+        .then((cache) => cache.addAll(urlsToCache))
     );
 });
 
-// Eliminar cachés antiguas cuando se active el nuevo SW
-self.addEventListener('activate', event => {
-    event.waitUntil(
-        caches.keys().then(cacheNames => {
-            return Promise.all(
-                cacheNames.filter(cache => cache !== CACHE_NAME)
-                    .map(cache => caches.delete(cache))
-            );
-        })
-    );
-});
-
-// Interceptar peticiones y responder con caché
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', (event) => {
     event.respondWith(
-        caches.match(event.request).then(response => {
-            return response || fetch(event.request);
-        })
+        caches.match(event.request)
+        .then((response) => response || fetch(event.request))
     );
-});
-
-
-self.addEventListener('message', (event) => {
-    if (event.data === 'skipWaiting') {
-        self.skipWaiting();
-    }
 });
