81e5b4999b44f165e112e13dfc73ebb128f6a549
nuserLatitude <noemdb@gmail.com>
2025-04-02 08:09:55 -0400

    wip 2025-04-02_08:09:55

--- resources/views/census.blade.php
+++ resources/views/census.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>Censo Escolar CFLA - {{ config('app.name') }}</title>
+    <title>Censo Escolar 2024 - 2025 || {{ config('app.name') }}</title>
 
     <!-- Livewire -->
     @livewireStyles
