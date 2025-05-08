fe5e32276646fddc9ec985be8c64e52dfe5fcc6a
nuserLatitude <noemdb@gmail.com>
2025-05-08 14:23:14 -0400

    wip 2025-05-08_14:23:14

--- resources/views/livewire/census/asistent/stepOne.blade.php
+++ resources/views/livewire/census/asistent/stepOne.blade.php
@@ -1,17 +1,17 @@
-<div class="mx-auto max-w-sm">
+<div class="max-w-sm mx-auto">
     <h2 class="mb-2 text-3xl font-bold text-white">Paso 1</h2>
 
     <div class="grid md:grid-cols-1">
-        
+
         @if (!$verificationCode)
 
             <p class="mb-4 text-gray-400">Ingresa tu dirección de correo electrónico</p>
-            <div class="space-y-2 mb-2">
+            <div class="mb-2 space-y-2">
                 <x-input wire:model="email" label="Correo Electrónico" placeholder="Tu email"
                     right-icon="at-symbol" description="Inform your full name" />
             </div>
 
-            <div class="space-y-2 mb-2">
+            <div class="mb-2 space-y-2">
                 <x-button wire:click="sendEmailCode" xl black label="Conecta tu email con nosotros" class="w-full"
                     type="email" />
             </div>
@@ -21,31 +21,31 @@
             <p class="mb-4 text-gray-400">Se ha enviado un código a tu correo electrónico</p>
 
             <x-card>
-                <p class="mb-4 text-gray-400 font-extrabold">
+                <p class="mb-4 font-extrabold text-gray-400">
                     Tu dirección de correo electrónico es:
                     <span class="font-normal">{{ $email ?? null }}</span>
-                </p>                    
+                </p>
             </x-card>
 
             <div class="py-2 my-2"></div>
 
-            <div class="space-y-2 mb-2">
-                <x-input 
+            <div class="mb-2 space-y-2">
+                <x-input
                     wire:model="input_code"
-                    label="Ingresa el código que haz recibido en tu dirección de correo electrónico" 
+                    label="Ingresa el código que has recibido en tu dirección de correo electrónico"
                     placeholder="Código"
-                    {{-- icon="hashtag"  --}} 
-                    suffix="#" 
+                    {{-- icon="hashtag"  --}}
+                    suffix="#"
                 />
             </div>
 
-            <div class="space-y-2 mb-2">
-                <x-button  
+            <div class="mb-2 space-y-2">
+                <x-button
                     wire:click="validateEmailCode"
                     xl
-                    positive 
-                    label="Valida tu dirección de correo electrónico" 
-                    class="w-full" 
+                    positive
+                    label="Valida tu dirección de correo electrónico"
+                    class="w-full"
                 />
             </div>
 
