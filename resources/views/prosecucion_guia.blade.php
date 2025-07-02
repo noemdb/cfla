af815ef8ebc5bb2f34a32c9a7d7d8945c8c3c475
noemdb <noemdb@gmail.com>
2025-07-02 04:51:27 -0400

    Update prosecucion_guia.blade.php

--- resources/views/prosecucion_guia.blade.php
+++ resources/views/prosecucion_guia.blade.php
@@ -85,12 +85,7 @@
                                         </svg>
                                         Ingresa tu número de cédula completo
                                     </li>
-                                    <li class="flex items-start">
-                                        <svg class="w-5 h-5 text-green-400 mr-3 mt-0.5 flex-shrink-0" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7"></path>
-                                        </svg>
-                                        Incluye la letra (V, E, J) y el número
-                                    </li>
+                                    
                                     <li class="flex items-start">
                                         <svg class="w-5 h-5 text-green-400 mr-3 mt-0.5 flex-shrink-0" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7"></path>
