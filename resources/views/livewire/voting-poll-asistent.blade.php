131cca3b1cff4c7dfce0234f69a89a1bbacd06a3
nuserLatitude <noemdb@gmail.com>
2025-07-07 10:58:07 -0400

    wip 2025-07-07_10:58:07

--- resources/views/livewire/voting-poll-asistent.blade.php
+++ resources/views/livewire/voting-poll-asistent.blade.php
@@ -204,7 +204,7 @@
 
         @if (!$hasVoted && !$isLoadingFingerprint && $currentPoll)
             <div class="mt-4 p-3 bg-yellow-700/20 border border-yellow-700/30 rounded-lg">
-                <p class="text-yellow-200 text-sm flex items-center">
+                <p class="text-yellow-200 text-sm flex items-center gap-2">
                     <svg class="w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 20 20">
                         <path fill-rule="evenodd"
                             d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
