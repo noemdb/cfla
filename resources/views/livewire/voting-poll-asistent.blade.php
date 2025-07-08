2b973165a830bc8d99ee84cd35b146cb94c135f4
nuserLatitude <noemdb@gmail.com>
2025-07-07 22:29:52 -0400

    wip 2025-07-07_22:29:52

--- resources/views/livewire/voting-poll-asistent.blade.php
+++ resources/views/livewire/voting-poll-asistent.blade.php
@@ -253,19 +253,20 @@
                             d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
                             clip-rule="evenodd"></path>
                     </svg>
-                    <strong>Información:</strong> Debes confirmar tu voto antes de poder continuar a la siguiente
+                    <strong>Información: &nbsp;</strong> Debes confirmar tu voto antes de poder continuar a la siguiente
                     encuesta.
                 </p>
             </div>
         @elseif($isExpired && $currentPoll)
             <div class="mt-4 p-3 bg-red-700/20 border border-red-700/30 rounded-lg">
-                <p class="text-red-200 text-sm flex items-center">
+                <p class="text-red-200 text-sm flex items-center gap-2">
                     <svg class="w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 20 20">
                         <path fill-rule="evenodd"
                             d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
                             clip-rule="evenodd"></path>
                     </svg>
-                    <strong>Encuesta Expirada:</strong> Esta encuesta ha finalizado. Puedes continuar a la siguiente
+                    <strong>Encuesta Expirada: &nbsp;</strong>
+                    Esta encuesta ha finalizado. Puedes continuar a la siguiente
                     encuesta disponible.
                 </p>
             </div>
