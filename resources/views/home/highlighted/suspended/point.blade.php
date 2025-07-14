ff21c7e1179f0843bbac5dd3db3a0da3f07e9f14
nuserLatitude <noemdb@gmail.com>
2025-07-13 21:11:03 -0400

    wip 2025-07-13_21:11:03

--- resources/views/home/highlighted/suspended/point.blade.php
+++ resources/views/home/highlighted/suspended/point.blade.php
@@ -9,10 +9,10 @@
         </h3>
     @endslot
 
-    <div class="container mx-auto px-4">
+    <div class="container mx-auto px-4 h-full">
         <div class="bg-yellow-100 border border-yellow-400 text-yellow-700 px-4 py-3 rounded relative" role="alert">
             <strong class="font-bold">⚠️ ¡Importante!</strong>
-            <span class="block sm:inline">El servicio de <strong>Botón de Pago Virtual (TPV)</strong>se encuentra
+            <span class="block sm:inline">El servicio de <strong>Botón de Pago Virtual (TPV)</strong> se encuentra
                 temporalmente suspendido.</span>
             <p class="text-sm mt-2">Agradecemos su comprensión y le pedimos disculpas por cualquier inconveniente.</p>
         </div>
