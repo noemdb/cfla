418e7c3409e2d953270525d59fa0649c1e6b0103
nuserLatitude <noemdb@gmail.com>
2025-07-16 08:20:28 -0400

    wip 2025-07-16_08:20:28

--- app/Livewire/CatchmentWizard.php
+++ app/Livewire/CatchmentWizard.php
@@ -36,8 +36,8 @@ class CatchmentWizard extends Component
     public $representant_cellphone; // Paso 3: Nombre del representante
     public $grade; // Paso 3: Grado/Nivel solicitado
     public $day_appointment; // Dia de la cita
-    public $day_appointment_start = '2025-07-16'; // Dia de la cita inical
-    public $day_appointment_end = '2025-07-15'; // Dia de la cita final
+    public $day_appointment_start = '2025-07-15'; // Dia de la cita inical
+    public $day_appointment_end = '2025-07-16'; // Dia de la cita final
     public $status_validate_code_email; // Dia de la cita final
 
     protected $listeners = ['hideVideo'];
