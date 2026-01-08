5aa4861dd8946973b2e75cf4628f95c2e8bddf0e
noemdb <noemdb@gmail.com>
2026-01-08 06:29:22 -0400

    Update footer.blade.php

--- resources/views/general/educational/competition/scoreboard/footer.blade.php
+++ resources/views/general/educational/competition/scoreboard/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="mt-auto bg-green-800 text-white rounded-lg shadow dark:bg-gray-800">
     <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
         <span class="text-sm text-gray-200 sm:text-center dark:text-gray-400">
-            © 2024 <a href="https://edusys.click/saefl" class="hover:underline font-extrabold">SAEFL</a>. Todos los
+            © 2024 <a href="https://edusys.click/saefl" class="hover:underline font-extrabold">EDUSYS.VE</a>. Todos los
             derechos reservados.
         </span>
         <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-200 dark:text-gray-400 sm:mt-0">
