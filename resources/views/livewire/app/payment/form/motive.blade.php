4040aaa6980ac2ba2c16698bb565ff6f1d6c33a9
nuserWork <noemdb@gmail.com>
2026-02-03 11:23:13 -0400

    wip 2026-02-03_11:23:13

--- resources/views/livewire/app/payment/form/motive.blade.php
+++ resources/views/livewire/app/payment/form/motive.blade.php
@@ -1,8 +1,27 @@
+@php
+        $name  = 'type_pay';
+        $model = 'payment.' . $name;
+        $label = $list_comment[$name];
+@endphp
+
+{{-- 
+<x-select
+    label="{{ $label }}"
+    placeholder="Seleccionar"
+    :options="$type_pay_list"
+    wire:model.live="{{ $model }}"
+    option-key-value
+    use-native-select
+/> 
+--}}
+
 <div class="pb-1">
-    @php $name = 'type_pay' ; $model = 'payment.'.$name; $label=$list_comment[$name] @endphp
-    <x-native-select label="{{$label}}" placeholder="Seleccionar" :options="$type_pay_list" wire:model.live="{{$model}}" option-key-value flip-options="false" searchable="true" />
+    <x-native-select
+        label="{{ $label }}"
+        placeholder="Seleccionar"
+        :options="$type_pay_list"
+        wire:model.live="{{ $model }}"
+        option-key-value
+        searchable
+    />
 </div>
-
-
-
-
