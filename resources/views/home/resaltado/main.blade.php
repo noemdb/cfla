046786f54dadac38fc4f5f364bad7b1d839dd305
nuserLatitude <noemdb@gmail.com>
2026-03-01 11:51:50 -0400

    wip 2026-03-01_11:51:50

--- resources/views/home/resaltado/main.blade.php
+++ resources/views/home/resaltado/main.blade.php
@@ -3,11 +3,10 @@
         class="relative w-full overflow-hidden rounded-3xl shadow-2xl border border-emerald-100/50 dark:border-emerald-900/20 bg-white dark:bg-gray-900/40 group">
 
         <!-- Aspect Ratio Container (3181 x 1000) -->
-        <div class="relative w-full" style="padding-top: 31.43%;">
-            <iframe src="https://drive.google.com/file/d/1XRGg_bZXzvlie_x0XCcDN72frkzJ-vxH/preview"
-                class="absolute top-0 left-0 w-full h-full border-none" allow="autoplay" title="Póster Resaltado"
-                loading="lazy">
-            </iframe>
+        <div class="relative w-full overflow-hidden" style="padding-top: 31.43%;">
+            <img src="{{ asset('image/resaltado/poster_horizontal.jpg') }}"
+                class="absolute top-0 left-0 w-full h-full object-cover rounded-3xl" alt="Póster Resaltado"
+                loading="lazy" />
         </div>
 
         <!-- Decorative Overlay (Optional, for a premium feel) -->
