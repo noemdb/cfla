4947a282b510884201270772a9134a0a26f3abe8
nuserLatitude <noemdb@gmail.com>
2026-04-01 09:21:00 -0400

    wip 2026-04-01_09:21:00

--- app/Livewire/CatchmentWizard.php
+++ app/Livewire/CatchmentWizard.php
@@ -1,22 +1,18 @@
 <?php
-
 namespace App\Livewire;
 
 use App\Models\app\Academy\Catchment;
 use App\Models\app\Entity\Autoridad;
 use App\Models\app\Entity\Institucion;
-use Illuminate\Support\Facades\Mail;
-use Illuminate\Support\Facades\Session;
 use App\Services\SendPulseService;
-use Livewire\Component;
-use WireUi\Traits\WireUiActions;
 use Barryvdh\DomPDF\Facade\Pdf;
 use Carbon\Carbon;
-use Illuminate\Support\Facades\DB;
-use SimpleSoftwareIO\QrCode\Facades\QrCode;
-use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Session;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\View;
+use Livewire\Component;
+use SimpleSoftwareIO\QrCode\Facades\QrCode;
+use WireUi\Traits\WireUiActions;
 
 class CatchmentWizard extends Component
 {
@@ -24,28 +20,28 @@ class CatchmentWizard extends Component
     use CatchmentValidate;
     use CatchmentUpdates;
 
-    public $catchment_id; // ID de la censo
-    public $currentStep = 1; // Paso actual del asistente
-    public $wizardFlow = null; // 'A' (No existe CI) o 'B' (Existe CI)
-    
-    public $email; // Paso 1 (Flow A): Correo electrónico
-    public $verificationCode = null; // Código de verificación
-    public $input_code; // Código generado para validar
-    public $catchmentsList = []; // Lista de censos encontrados para este representante
-    public $firstname; // Paso 2: Nombre completo del niño/a
-    public $lastname; // Paso 2: Nombre completo del niño/a
-    public $date_birth; // Paso 2: Fecha de nacimiento
-    public $representant_name; // Paso 3: Nombre del representante
-    public $representant_ci; // Paso 3: CI del representante
-    public $representant_phone; // Paso 3: WhatsApp del representante
-    public $representant_cellphone; // Paso 3: Nombre del representante
-    public $grade; // Paso 3: Grado/Nivel solicitado
-    public $day_appointment; // Dia de la cita
-    public $day_appointment_start = '2026-02-18'; // Dia de la cita inical
-    public $day_appointment_end = '2026-07-31'; // Dia de la cita final
-    public $status_validate_code_email; // Dia de la cita final
+    public $catchment_id;       // ID de la censo
+    public $currentStep = 1;    // Paso actual del asistente
+    public $wizardFlow  = null; // 'A' (No existe CI) o 'B' (Existe CI)
 
-    protected $listeners = ['hideVideo'];
+    public $email;                                // Paso 1 (Flow A): Correo electrónico
+    public $verificationCode = null;              // Código de verificación
+    public $input_code;                           // Código generado para validar
+    public $catchmentsList = [];                  // Lista de censos encontrados para este representante
+    public $firstname;                            // Paso 2: Nombre completo del niño/a
+    public $lastname;                             // Paso 2: Nombre completo del niño/a
+    public $date_birth;                           // Paso 2: Fecha de nacimiento
+    public $representant_name;                    // Paso 3: Nombre del representante
+    public $representant_ci;                      // Paso 3: CI del representante
+    public $representant_phone;                   // Paso 3: WhatsApp del representante
+    public $representant_cellphone;               // Paso 3: Nombre del representante
+    public $grade;                                // Paso 3: Grado/Nivel solicitado
+    public $day_appointment;                      // Dia de la cita
+    public $day_appointment_start = '2026-02-18'; // Dia de la cita inical
+    public $day_appointment_end   = '2026-07-31'; // Dia de la cita final
+    public $status_validate_code_email;           // Dia de la cita final
+
+    protected $listeners   = ['hideVideo'];
     public bool $showVideo = true; // Estado inicial: mostrar video
     public function hideVideo()
     {
@@ -65,38 +61,37 @@ class CatchmentWizard extends Component
 
     public function restart()
     {
-        $this->currentStep = 1;
-        $this->currentStep = 1;
-        $this->catchment_id = null;
-        $this->wizardFlow = null;
-        $this->email = null;
-        $this->verificationCode = null;
-        $this->input_code = null;
-        $this->catchmentsList = [];
-        $this->firstname = null;
-        $this->lastname = null;
-        $this->date_birth = null;
-        $this->representant_name = null;
-        $this->representant_ci = null;
-        $this->representant_phone = null;
-        $this->representant_cellphone = null;
-        $this->grade = null;
-        $this->day_appointment = null;
+        $this->currentStep                = 1;
+        $this->currentStep                = 1;
+        $this->catchment_id               = null;
+        $this->wizardFlow                 = null;
+        $this->email                      = null;
+        $this->verificationCode           = null;
+        $this->input_code                 = null;
+        $this->catchmentsList             = [];
+        $this->firstname                  = null;
+        $this->lastname                   = null;
+        $this->date_birth                 = null;
+        $this->representant_name          = null;
+        $this->representant_ci            = null;
+        $this->representant_phone         = null;
+        $this->representant_cellphone     = null;
+        $this->grade                      = null;
+        $this->day_appointment            = null;
         $this->status_validate_code_email = null;
     }
 
     public function mount()
     {
-        // $catchments = DB::connection('s2526')->table('catchments')->get(); dd($catchments);
-        $today = now()->toDateString();
-        $this->day_appointment = $today;
+        $today                       = now()->toDateString();
+        $this->day_appointment       = $today;
         $this->day_appointment_start = ($today > $this->day_appointment_start) ? $today : $this->day_appointment_start;
     }
 
     public function searchByCi()
     {
         $this->validate([
-            'representant_ci' => 'required|string|min:6|max:15'
+            'representant_ci' => 'required|string|min:6|max:15',
         ], [
             'representant_ci.required' => 'La cédula de identidad es requerida.',
         ]);
@@ -106,15 +101,15 @@ class CatchmentWizard extends Component
             ->get();
 
         if (count($this->catchmentsList) === 0) {
-            $this->wizardFlow = 'A';
+            $this->wizardFlow  = 'A';
             $this->currentStep = 2; // Avance a stepEmail
-            $this->notification()->warning(
-                $title = 'Cédula no registrada',
-                $description = 'No posees historiales. Por favor, procede a validar tu correo electrónico para un nuevo registro.'
-            );
+                                    // $this->notification()->warning(
+                                    //     $title = 'Cédula no registrada',
+                                    //     $description = 'No posees historiales. Por favor, procede a validar tu correo electrónico para un nuevo registro.'
+                                    // );
         } else {
-            $this->wizardFlow = 'B';
-            $this->email = $this->catchmentsList[0]->email ?? null;
+            $this->wizardFlow  = 'B';
+            $this->email       = $this->catchmentsList[0]->email ?? null;
             $this->currentStep = 2; // Avance a stepList
             $this->notification()->success(
                 $title = 'Estatus recuperado',
@@ -132,9 +127,9 @@ class CatchmentWizard extends Component
 
         try {
             $html = View::make('emails.verification-code', [
-                'code' => $this->verificationCode
+                'code' => $this->verificationCode,
             ])->render();
-            
+
             $result = $sendPulseService->sendEmail(
                 to: $this->email,
                 subject: 'Código de verificación',
@@ -164,7 +159,7 @@ class CatchmentWizard extends Component
     public function validateEmailCode()
     {
         if ($this->input_code == Session::get('email_code')) {
-            $this->currentStep = 3; // Avanza a paso 3: Estudiante
+            $this->currentStep                = 3; // Avanza a paso 3: Estudiante
             $this->status_validate_code_email = true;
             $this->notification()->success(
                 $title = 'Excelente!',
@@ -187,8 +182,8 @@ class CatchmentWizard extends Component
     public function validateStudent()
     {
         $this->validate([
-            'firstname' => 'required|string|max:100',
-            'lastname' => 'required|string|max:100',
+            'firstname'  => 'required|string|max:100',
+            'lastname'   => 'required|string|max:100',
             'date_birth' => 'required|date|before:today',
         ]);
 
@@ -212,23 +207,23 @@ class CatchmentWizard extends Component
     {
         $date = $this->validate();
 
-        $time = Carbon::now()->timestamp;
+        $time   = Carbon::now()->timestamp;
         $random = mt_rand(10000, 99999);
-        $token = substr($time . $random, -10);
+        $token  = substr($time . $random, -10);
 
         $catchment = Catchment::create([
-            'user_id' => auth()->id(),
-            'email' => $this->email,
-            'day_appointment' => $this->day_appointment,
-            'representant_ci' => $this->representant_ci,
-            'representant_name' => $this->representant_name,
-            'representant_phone' => $this->representant_phone,
+            'user_id'                => auth()->id(),
+            'email'                  => $this->email,
+            'day_appointment'        => $this->day_appointment,
+            'representant_ci'        => $this->representant_ci,
+            'representant_name'      => $this->representant_name,
+            'representant_phone'     => $this->representant_phone,
             'representant_cellphone' => $this->representant_cellphone,
-            'grade' => $this->grade,
-            'firstname' => $this->firstname,
-            'lastname' => $this->lastname,
-            'date_birth' => $this->date_birth,
-            'token' => $token,
+            'grade'                  => $this->grade,
+            'firstname'              => $this->firstname,
+            'lastname'               => $this->lastname,
+            'date_birth'             => $this->date_birth,
+            'token'                  => $token,
         ]);
 
         $this->catchment_id = ($catchment) ? $catchment->id : null;
@@ -251,25 +246,25 @@ class CatchmentWizard extends Component
 
     public function downloadPDF($catchment_id)
     {
-        $catchment = Catchment::findOrFail($catchment_id);
+        $catchment   = Catchment::findOrFail($catchment_id);
         $institution = Institucion::first();
-        $autoridad1 = Autoridad::getAuthority('DIRECTOR GENERAL Y ADMINISTRATIVO');
-        $autoridad2 = Autoridad::getAuthority('DIRECTORA'); //dd($institution,$autoridad1,$autoridad2);
-        $data = [
-            'firstname' => $catchment->firstname,
-            'lastname' => $catchment->lastname,
-            'date_birth' => $catchment->date_birth,
-            'representant_name' => $catchment->representant_name,
-            'representant_ci' => $catchment->representant_ci,
-            'representant_phone' => $catchment->representant_phone,
+        $autoridad1  = Autoridad::getAuthority('DIRECTOR GENERAL Y ADMINISTRATIVO');
+        $autoridad2  = Autoridad::getAuthority('DIRECTORA'); //dd($institution,$autoridad1,$autoridad2);
+        $data        = [
+            'firstname'              => $catchment->firstname,
+            'lastname'               => $catchment->lastname,
+            'date_birth'             => $catchment->date_birth,
+            'representant_name'      => $catchment->representant_name,
+            'representant_ci'        => $catchment->representant_ci,
+            'representant_phone'     => $catchment->representant_phone,
             'representant_cellphone' => $catchment->representant_cellphone,
-            'day_appointment' => $catchment->day_appointment,
+            'day_appointment'        => $catchment->day_appointment,
             // 'grado_id' => $catchment->grado_id,
-            'grado' => $catchment->grado,
-            'institution' => $institution,
-            'autoridad1' => $autoridad1,
-            'autoridad2' => $autoridad2,
-            'qrCode' => $this->generateQrCodePDF($catchment->token),
+            'grado'                  => $catchment->grado,
+            'institution'            => $institution,
+            'autoridad1'             => $autoridad1,
+            'autoridad2'             => $autoridad2,
+            'qrCode'                 => $this->generateQrCodePDF($catchment->token),
         ];
 
         // Crear el PDF
@@ -285,7 +280,7 @@ class CatchmentWizard extends Component
     public function generateQrCode($catchment_id)
     {
         $catchment = Catchment::findOrFail($catchment_id);
-        $pdfUrl = route('catchment.download.pdf', $catchment->token); // Ruta que descarga el PDF
+        $pdfUrl    = route('catchment.download.pdf', $catchment->token); // Ruta que descarga el PDF
         return QrCode::size(200)->generate($pdfUrl);
     }
 
