7b664132889b3231e4a47a7f11dcbf0113aef5aa
nuserLatitude <noemdb@gmail.com>
2026-04-10 16:09:28 -0400

    wip 2026-04-10_16:09:27

--- resources/views/livewire/census/asistent/stepCi.blade.php
+++ resources/views/livewire/census/asistent/stepCi.blade.php
@@ -3,7 +3,7 @@
 
     <div class="grid md:grid-cols-1">
         <p class="mb-4 text-gray-400">Por favor, ingresa tu cédula de identidad para consultar el estatus del censo de
-            tus representados, o registrar uno nuevo.</p>
+            tus representados, o registrar uno nuevo</p>
 
         <div class="mb-4 space-y-2">
             <x-input wire:model="representant_ci" label="Cédula de Identidad" placeholder="Ej: 12345678"
@@ -12,11 +12,8 @@
 
         <div class="mb-2 space-y-2">
 
-
             <x-button wire:click="searchByCi" xl positive label="Consultar"
                 class="w-full !bg-green-800 hover:!bg-green-900 border-2 !border-green-900 shadow-lg" type="button" />
-
-
         </div>
     </div>
 </div>
